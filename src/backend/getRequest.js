d99477ab272662588f6d79640c81db79194d4635
Sarah-faisal <sarahpervaizpk@gmail.com>
2023-12-11 17:37:51 +0500

    env file updated

--- src/backend/getRequest.js
+++ src/backend/getRequest.js
@@ -1,13 +1,13 @@
 const getRequest = async (location , dt )=>{
     try {
     let apikey = process.env.REACT_APP_APIKEY;
-    
+
     const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${location}&days=3&dt=${dt}`;
 
     const options = {
       headers: {
         'Content-Type': 'application/json',
-        'X-RapidAPI-Key': '10a4ba1cd4msh1ea4162312a54efp1c67bdjsnec864ab7b06e',
+        'X-RapidAPI-Key': `${apikey}`,
         'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'       
       },
     };
